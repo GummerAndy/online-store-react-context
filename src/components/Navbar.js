9202d7ea56de97b398266888b1de28899fa7cd80
Andy Gummer <gummer.andy@gmail.com>
2020-01-27 17:30:28 +0000

    Initial commit with navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../logo.svg'
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                <Link to='/'>
+                    <img src={logo} alt="store" className="navbar-brand" />
+                </Link>
+                <ul className="navbar-nav align-items-center">
+                    <li className="nav-item ml-5"></li>
+                    <Link to="/" className="nav-link">
+                        Products
+                    </Link>
+                </ul>
+                <Link to="/cart" className="ml-auto">
+                    <button><i className="fas fa-cart-plus" />My Cart</button>
+                </Link>
+            </nav>
+        )
+    }
+}
