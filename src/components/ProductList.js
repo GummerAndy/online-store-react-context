e362979a0ad1247b940e86c9224234b8a67d46f2
Andy Gummer <gummer.andy@gmail.com>
2020-01-28 15:14:55 +0000

    Get data.js array to show in productlist console

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title'
-// import { storeProducts } from '../data';
+import { storeProducts } from '../data';
 
 export default class ProductList extends Component {
     state = {
-        // products: { storeProducts }
+        products: { storeProducts }
     }
     render() {
-        // console.log(this.state.storeProducts)
+        console.log(this.state.products)
         return (
             <React.Fragment>
                 <div className="py-5">
