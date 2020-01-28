6a163d237d04f928819dedd7050c3ee9fdd37b47
Andy Gummer <gummer.andy@gmail.com>
2020-01-28 15:08:24 +0000

    Added NavWrapper and Title as functional component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,22 @@
-import React, { Component } from 'react'
-import Product from './Product'
+import React, { Component } from 'react';
+import Product from './Product';
+import Title from './Title'
+// import { storeProducts } from '../data';
 
 export default class ProductList extends Component {
+    state = {
+        // products: { storeProducts }
+    }
     render() {
+        // console.log(this.state.storeProducts)
         return (
-            <div>
-                <Product />
-            </div>
-        )
+            <React.Fragment>
+                <div className="py-5">
+                    <div className="container">
+                        <Title name="Our" title="Products" />
+                    </div>
+                </div>
+            </React.Fragment>
+        );
     }
 }
