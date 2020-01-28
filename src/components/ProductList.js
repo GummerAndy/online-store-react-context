893668349f1962efc373d6582d73f1c49c5a52cd
Andy Gummer <gummer.andy@gmail.com>
2020-01-28 16:22:19 +0000

    Added loop for Productlist from context

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -9,7 +9,6 @@ export default class ProductList extends Component {
         products: { storeProducts }
     }
     render() {
-        console.log(this.state.products)
         return (
             <React.Fragment>
                 <div className="py-5">
@@ -18,7 +17,9 @@ export default class ProductList extends Component {
                         <div className="row">
                             <ProductConsumer>
                                 {(value) => {
-                                    return <h1>value</h1>
+                                    return value.products.map(product => {
+                                        return <Product key={product.id} product={product} />
+                                    })
                                 }}
                             </ProductConsumer>
                         </div>
--- src/context.js
+++ src/context.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import { storeProducts, detailProduct } from './data';
 const ProductContext = React.createContext();
+
 // Provider;
 // Consumer;
+
 class ProductProvider extends Component {
+    state = {
+        products: storeProducts,
+        detailProduct: detailProduct
+    }
+    handleDetail = () => {
+        console.log("Hello from Detail")
+    }
+
+    addToCart = () => {
+        console.log("Hello from Add to Cart")
+    }
     render() {
         return (
-            <ProductContext.Provider value="Hello from Phone App">
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail: this.handleDetail,
+                addToCart: this.addToCart
+            }}>
                 {this.props.children}
             </ProductContext.Provider>
         );
