e516fe85c66281ae25d879c6cbf50be0e20ae470
Andy Gummer <gummer.andy@gmail.com>
2020-01-29 08:06:59 +0000

    Added conditional inCart button to product

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,10 +9,28 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={() => console.log('you clicked on the image container')}>
+                    <div
+                        className="img-container p-5"
+                        onClick={() => console.log('you clicked on the image container')}>
                         <Link to="/details">
-                            <img src={img} alt="Product" className="card-image-top" />
+                            <img
+                                src={img}
+                                alt="Product"
+                                className="card-image-top" />
                         </Link>
+                        <button
+                            className="card-btn"
+                            disabled={inCart ? true : false}
+                            onClick={() => {
+                                console.log("added to cart");
+                            }}>
+                            {inCart ? (
+                                <p className="text-capitalize mb-0" disabled>
+                                    {" "}
+                                    in inCart
+                            </p>
+                            ) : (<i className="fas fa-cart-plus" />)}
+                        </button>
                     </div>
                 </div>
             </ProductWrapper>
