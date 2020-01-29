7a35fc7bdd8b5afc3b2cc2c96a14108476669938
Andy Gummer <gummer.andy@gmail.com>
2020-01-29 08:11:59 +0000

    added product footer

--- src/components/Product.js
+++ src/components/Product.js
@@ -32,6 +32,15 @@ export default class Product extends Component {
                             ) : (<i className="fas fa-cart-plus" />)}
                         </button>
                     </div>
+                    {/* {card footer} */}
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">
+                            {title}
+                        </p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">€{price}</span>
+                        </h5>
+                    </div>
                 </div>
             </ProductWrapper>
         )
