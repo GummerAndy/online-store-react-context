1b03d644f069b7f523f8a4794fec2b9313f2924e
Andy Gummer <gummer.andy@gmail.com>
2020-02-02 16:23:08 +0000

    connected products with corresponding detail page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { ProductConsumers } from '../context'
+import { ProductConsumer } from '../context'
 
 export default class Product extends Component {
     render() {
@@ -9,29 +9,33 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div
-                        className="img-container p-5"
-                        onClick={() => console.log('you clicked on the image container')}>
-                        <Link to="/details">
-                            <img
-                                src={img}
-                                alt="Product"
-                                className="card-img-top" />
-                        </Link>
-                        <button
-                            className="cart-btn"
-                            disabled={inCart ? true : false}
-                            onClick={() => {
-                                console.log("added to cart");
-                            }}>
-                            {inCart ? (
-                                <p className="text-capitalize mb-0" disabled>
-                                    {" "}
-                                    in inCart
+                    <ProductConsumer>
+                        {(value) => (<div
+                            className="img-container p-5"
+                            onClick={() =>
+                                value.handleDetail(id)}>
+                            <Link to="/details">
+                                <img
+                                    src={img}
+                                    alt="Product"
+                                    className="card-img-top" />
+                            </Link>
+                            <button
+                                className="cart-btn"
+                                disabled={inCart ? true : false}
+                                onClick={() => {
+                                    value.addToCart(id);
+                                }}>
+                                {inCart ? (
+                                    <p className="text-capitalize mb-0" disabled>
+                                        {" "}
+                                        in inCart
                             </p>
-                            ) : (<i className="fas fa-cart-plus" />)}
-                        </button>
-                    </div>
+                                ) : (<i className="fas fa-cart-plus" />)}
+                            </button>
+                        </div>)}
+
+                    </ProductConsumer>
                     {/* {card footer} */}
                     <div className="card-footer d-flex justify-content-between">
                         <p className="align-self-center mb-0">
