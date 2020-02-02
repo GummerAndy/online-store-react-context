cd8e8a23f6e0eb9001818e68f2b76ce84ea8e7ad
Andy Gummer <gummer.andy@gmail.com>
2020-02-02 15:55:32 +0000

    added conditional to styled component for add to cart button

--- src/components/Details.js
+++ src/components/Details.js
@@ -47,6 +47,7 @@ export default class Details extends Component {
                                             <ButtonContainer>Back to products</ButtonContainer>
                                         </Link>
                                         <ButtonContainer
+                                            cart
                                             disabled={inCart ? true : false}
                                             onClick={() => {
                                                 value.addToCart(id);
