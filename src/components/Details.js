0e94e3e5c084f766c2944cbea87213c91e44c558
Andy Gummer <gummer.andy@gmail.com>
2020-02-02 20:36:09 +0000

    added modal for added to cart

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,59 +7,62 @@ export default class Details extends Component {
     render() {
         return (
             <div>
-                <ProductConsumer>{value => {
-                    const {
-                        id,
-                        company,
-                        img,
-                        info,
-                        price,
-                        title,
-                        inCart } = value.detailProduct;
-                    return (
-                        <div className="container py-5">
-                            {/* Title */}
-                            <div className="row">
-                                <div className="col-10 mx-auto text-centered text-slanted text-blue my-5">
-                                    <h1>{title}</h1>
+                <ProductConsumer>
+                    {value => {
+                        const {
+                            id,
+                            company,
+                            img,
+                            info,
+                            price,
+                            title,
+                            inCart
+                        } = value.detailProduct;
+                        return (
+                            <div className="container py-5">
+                                {/* Title */}
+                                <div className="row">
+                                    <div className="col-10 mx-auto text-centered text-slanted text-blue my-5">
+                                        <h1>{title}</h1>
+                                    </div>
                                 </div>
-                            </div>
-                            {/* End title */}
-                            <div className="row">
-                                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                                    <h2>model : {title}</h2>
-                                    <h4 className="text-title text-uppercase text-muted mt-3 mb2">
-                                        made by : <span className="text-uppercase">{company}</span>
-                                    </h4>
-                                    <h4 className="text-blue">
-                                        <strong>
-                                            price : <span>$</span>
-                                            {price}
-                                        </strong>
-                                    </h4>
-                                    <p className="text-capitalize font-weight-bold mt-3 mb-0">
-                                        some info about the product:
+                                {/* End title */}
+                                <div className="row">
+                                    <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                                        <h2>model : {title}</h2>
+                                        <h4 className="text-title text-uppercase text-muted mt-3 mb2">
+                                            made by : <span className="text-uppercase">{company}</span>
+                                        </h4>
+                                        <h4 className="text-blue">
+                                            <strong>
+                                                price : <span>$</span>
+                                                {price}
+                                            </strong>
+                                        </h4>
+                                        <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                                            some info about the product:
                                     </p>
-                                    <p className="text-muted lead">{info}</p>
-                                    {/* Buttons */}
-                                    <div className="">
-                                        <Link to="/">
-                                            <ButtonContainer>Back to products</ButtonContainer>
-                                        </Link>
-                                        <ButtonContainer
-                                            cart
-                                            disabled={inCart ? true : false}
-                                            onClick={() => {
-                                                value.addToCart(id);
-                                            }}>
-                                            {inCart ? 'in Cart' : 'Add to Cart'}
-                                        </ButtonContainer>
+                                        <p className="text-muted lead">{info}</p>
+                                        {/* Buttons */}
+                                        <div className="">
+                                            <Link to="/">
+                                                <ButtonContainer>Back to products</ButtonContainer>
+                                            </Link>
+                                            <ButtonContainer
+                                                cart
+                                                disabled={inCart ? true : false}
+                                                onClick={() => {
+                                                    value.addToCart(id);
+                                                    value.openModal(id);
+                                                }}>
+                                                {inCart ? 'in Cart' : 'Add to Cart'}
+                                            </ButtonContainer>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                }}</ProductConsumer>
+                        )
+                    }}</ProductConsumer>
             </div>
         )
     }
