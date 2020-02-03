3219a8fc58791dce780ae2464938075118b9aa4f
Andy Gummer <gummer.andy@gmail.com>
2020-02-03 14:03:16 +0000

    added remove functionality

--- src/context.js
+++ src/context.js
@@ -84,7 +84,28 @@ class ProductProvider extends Component {
     }
 
     removeItem = (id) => {
-        console.log("This is the removeItem method")
+        let tempProducts = [...this.state.products];
+        let tempCart = [...this.state.cart];
+
+        tempCart = tempCart.filter(item => item.id !== id);
+
+        const index = tempProducts.indexOf(this.getItem(id));
+        let removedProduct = tempProducts[index];
+        removedProduct.inCart = false;
+        removedProduct.count = 0;
+        removedProduct.total = 0;
+
+        this.setState(
+            () => {
+                return {
+                    cart: [...tempCart],
+                    products: [...tempProducts]
+                };
+            },
+            () => {
+                this.addTotals();
+            }
+        )
     }
 
     clearCart = () => {
