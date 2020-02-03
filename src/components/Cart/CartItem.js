c9fbec94f8c689bba7639f74f90f44fae5a1ad94
Andy Gummer <gummer.andy@gmail.com>
2020-02-03 11:28:23 +0000

    added buttons to cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -20,13 +20,16 @@ export default function CartItem({ item, value }) {
                 <span className="d-lg-none">price : </span>
                 {price}
             </div>
-            {/* <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
+            <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
                 <div className="d-flex justify-content-center">
                     <div>
-
+                        <span className="btn btn-black mx-1" onClick={() => decrement(id)}>-</span>
+                        <span className="btn btn-black mx-1">{count}</span>
+                        <span className="btn btn-black mx-1" onClick={() => increment(id)}>+</span>
                     </div>
                 </div>
-            </div> */}
+            </div>
+            {/*  */}
         </div>
     )
 }
