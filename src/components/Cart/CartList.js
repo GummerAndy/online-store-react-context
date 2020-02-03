d4fea0eeae55fcd9a1bba3ba0cbec1c7561fcf71
Andy Gummer <gummer.andy@gmail.com>
2020-02-03 11:41:02 +0000

    added remove item button

--- src/components/Cart/CartList.js
+++ src/components/Cart/CartList.js
@@ -7,7 +7,7 @@ export default function CartList({ value }) {
         <div className="container-fluid">
             {cart.map(item => {
                 return <CartItem
-                    key={item}
+                    key={item.id}
                     item={item}
                     value={value} />
             })}
